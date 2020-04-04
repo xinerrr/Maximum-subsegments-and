934601b228650b81797efa41a747e2f8c2b67b03
xinerrr <62320330+xinerrr@users.noreply.github.com>
2020-04-04 12:14:19 +0800

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,30 +1,44 @@
-#include<iostream>
+#include <cstdio>
+#include <iostream>
 using namespace std;
-int i, j, n, k[50];
-int Max(int k[], int n)
-{
-	int aSum, bSum = 0;
-	for (int i = 0; i < n; i++)
-	{
-		aSum = 0;
-		for (int j = i; j < n; j++)
-		{
-			aSum += k[j];
-			if (aSum > bSum)
-				bSum = aSum;
-		}
-	}
-	return bSum;
-}
+
 int main()
 {
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++)
-		cin >> k[i];
-	if (n <= 0)
-		cout << 0;
-	else
-		cout << Max(k, n);
-	return 0;
+    int num = 0;
+    int Max = 0;
+    int temp;
+    cin >> num;
+    if (num <= 0)
+    {
+        cout << "error" <<endl;
+    }
+    else
+    {
+        int* arra = new int[num];
+        for (int i = 0; i < num; i++)
+        {
+            cin >> arra[i];
+        }
+        for (int i = 0; i < num; i++)
+        {
+            for (int k = 0; k < num; k++)
+            {
+                temp = arra[i];
+                if (temp > Max)
+                {
+                    Max = temp;
+                }
+                for (int j = i + 1; j <= k; j++)
+                {
+                    temp = temp + arra[j];
+                    if (temp > Max)
+                    {
+                        Max = temp;
+                    }
+                }
+            }
+        }
+        cout << Max << endl;
+        delete[] arra;
+    }
 }
